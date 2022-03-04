fbdff11b017c23b49c53506b1b19a66f42fff1a5
Cleber Andrade <cleberandrade@gmail.com>
2022-03-04 14:50:30 -0300

    alterações direto na branch principal

--- Project3.dpr
+++ Project3.dpr
@@ -3,12 +3,14 @@ program Project3;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Unit3 in 'Unit3.pas' {Form3};
+  Unit3 in 'Unit3.pas' {Form3},
+  Unit4 in 'Unit4.pas' {Form4};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TForm4, Form4);
   Application.Run;
 end.
